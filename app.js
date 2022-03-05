2460d1a53193beb562520e6fe1d338869abc8047
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-05 16:25:31 +0900

    cors 수정

--- app.js
+++ app.js
@@ -23,18 +23,20 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-app.use(express.json());
-app.use(express.urlencoded({extended: false})); // API 요청에서 받은 body 값을 파싱(해석)하는 역할을 수행하는 것이 bodyParser
-app.use('/api', [authRouter, planRouter, commentRouter])
-
-
 const corsOptions = {
     origin: '*',
     // credentials: true
 };
 
 app.use(cors(corsOptions));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); // API 요청에서 받은 body 값을 파싱(해석)하는 역할을 수행하는 것이 bodyParser
+app.use('/api', [authRouter, planRouter, commentRouter])
+
+
+
 
 app.listen(3000, () => {
     console.log( new Date().toLocaleString() , '서버가 3000포트로 요청을 받을 준비가 됐어요');
