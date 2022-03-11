0bb45060186af49f681e7f5f8bbeaa15193d5d31
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-12 02:27:44 +0900

    bookmark 수정

--- controller/bookmark.js
+++ controller/bookmark.js
@@ -3,7 +3,7 @@ const BookMarksService = require('../services/bookmarks');
 const findBookmark = async (req, res) => {
     const { userId } = res.locals.user;
 
-    const findBookmark = await BookMarkService.getBookmarkById({ userId });
+    const findBookmark = await BookMarksService.getBookmarkById({ userId });
 
     return res.json({ result: 'success', plans: findBookmark });
 };
--- services/bookmarks.js
+++ services/bookmarks.js
@@ -2,22 +2,14 @@ const User = require('../models/user');
 const Bookmark = require('../models/bookmark');
 
 const getBookmarkByUserId = async ({ userId }) => {
-
     const findBookmarks = await Bookmark.find({ userId }).populate({
         path: 'planId',
         populate: { path: 'userId' },
     });
 
     return findBookmarks;
-
-}
-
-
-
-
-
-
+};
 
 module.exports = {
-    getBookmarkByUserId
+    getBookmarkByUserId,
 };
