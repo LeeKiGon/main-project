e43b31412dd44ac04a966e7e8a20e2bad390f699
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-15 14:05:58 +0900

    auth uqdate

--- controller/auth.js
+++ controller/auth.js
@@ -1,52 +1,53 @@
-const userService = require("../services/auth");
-const passport = require("passport");
-const jwt = require("jsonwebtoken");
+const userService = require('../services/auth');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const { JWT_SECRET_KEY } = process.env;
 
 const updateUserInfo = async (req, res, next) => {
-    try{
-    const { userId } = res.locals.user;
-    const { nickname } = req.body;
-    const { profile_img } = req.file
+    try {
+        const { userId } = res.locals.user;
+        const { nickname } = req.body;
+        const { profile_img } = req.file;
 
-    await userService.updateUserInfo({ userId, nickname, profile_img });
-    res.status(200).json({ result:'success', message: '업데이트 완료되었습니다.'});
-    }catch (error){
-        next(error)
+        await userService.updateUserInfo({ userId, nickname, profile_img });
+        res.status(200).json({
+            result: 'success',
+            message: '업데이트 완료되었습니다.',
+        });
+    } catch (error) {
+        next(error);
     }
-    
-}
+};
 
 const getMyInfo = async (req, res, next) => {
     try {
         const { user } = res.locals;
-    res.status(200).json({
-        result: 'success',
-        userId: user._id,
-        snsId: user.snsId,
-        email: user.email,
-        nickname: user.nickname,
-        userImg: user.profile_img,
-    });
-
+        res.status(200).json({
+            result: 'success',
+            userId: user._id,
+            snsId: user.snsId,
+            email: user.email,
+            nickname: user.nickname,
+            userImg: user.profile_img,
+        });
     } catch (error) {
-        next(error)
+        next(error);
     }
-    
-}
+};
 
 const getUserInfo = async (req, res, next) => {
-    try{
-    const { user } = res.locals;
-    const { userId } = req.params;
-    const findUserInfo = await userService.findUserInfoByUserId({ myUserId : user.userId, otherUserId : userId });
-    res.status(200).json({ result:'success', userInfo : findUserInfo });
-    }catch (error){
-        next(error)
+    try {
+        const { user } = res.locals;
+        const { userId } = req.params;
+        const findUserInfo = await userService.findUserInfoByUserId({
+            myUserId: user.userId,
+            otherUserId: userId,
+        });
+        res.status(200).json({ result: 'success', userInfo: findUserInfo });
+    } catch (error) {
+        next(error);
     }
-    
-}
-
+};
 
 const kakaoCallback = (req, res, next) => {
     passport.authenticate('kakao', { failureRedirect: '/' }, (err, user) => {
@@ -55,6 +56,6 @@ const kakaoCallback = (req, res, next) => {
 
         res.json({ token, userId: user.email });
     })(req, res, next);
-}
+};
 
-module.exports = { kakaoCallback, getMyInfo, getUserInfo, updateUserInfo }
+module.exports = { kakaoCallback, getMyInfo, getUserInfo, updateUserInfo };
--- services/auth.js
+++ services/auth.js
@@ -1,21 +1,33 @@
-const User = require("../models/user");
+const User = require('../models/user');
 
 const updateUserInfo = async ({ userId, nickname, profile_img }) => {
-    const findUserByUserId = await User.findOneAndUpdate({ _id: userId }, {nickname, profile_img})
-
-    return;
+    try {
+        await User.findOneAndUpdate({ _id: userId }, { nickname, profile_img });
+        return;
+    } catch (error) {
+        throw error;
+    }
 };
 
 const findUserInfoByUserId = async ({ myUserId, otherUserId }) => {
-
-    const loginUserInfo = await User.findOne({ _id: myUserId }).populate('plans');
-    const otherUserInfo = await User.find({ _id: otherUserId }).populate({path: 'plans', match : {status:'공개'}})
-    if(loginUserInfo.userId === otherUserInfo.userId) {
-        return loginUserInfo;
+    try {
+        const loginUserInfo = await User.findOne({ _id: myUserId }).populate(
+            'plans'
+        );
+        const otherUserInfo = await User.find({ _id: otherUserId }).populate({
+            path: 'plans',
+            match: { status: '공개' },
+        });
+        if (loginUserInfo.userId === otherUserInfo.userId) {
+            return loginUserInfo;
+        }
+        return otherUserInfo;
+    } catch (error) {
+        throw error;
     }
-    return otherUserInfo;
 };
 
-
-
-module.exports = { };
+module.exports = {
+    findUserInfoByUserId,
+    updateUserInfo,
+};
